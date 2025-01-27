3870fbf2e0d69edfa58a8506e6a067f2611837de
surajk95 <surajkukati95@gmail.com>
2025-01-27 14:26:53 +0530

    updated title

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="w-full grid grid-rows-[20px_1fr_20px] min-h-screen p-2 pb-20 gap-16 sm:p-5 font-[family-name:var(--font-geist-sans)]">
        <header className="flex items-center justify-start gap-5 h-14 border-b">
-        <h1 className="text-xl font-bold">Frame Builder</h1>
+        <h1 className="text-xl font-bold">Frames Builder</h1>
         <ThemeToggle />
       </header>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
